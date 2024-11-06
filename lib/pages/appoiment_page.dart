304a29f2d462a1c75b9f2704bb81bfdefd843fbe
fouzan. p <fouzanparammal2@gmail.com>
2024-11-06 18:33:19 +0530

    added

---
+++ lib/pages/appoiment_page.dart
@@ -0,0 +1,14 @@
+// appointment_page.dart
+import 'package:flutter/material.dart';
+
+class AppointmentPage extends StatelessWidget {
+  const AppointmentPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Appointment Page", style: TextStyle(fontSize: 24)),
+    );
+  }
+}
+
---
+++ lib/pages/message_page.dart
@@ -0,0 +1,15 @@
+
+
+// message_page.dart
+import 'package:flutter/material.dart';
+
+class MessagePage extends StatelessWidget {
+  const MessagePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Message Page", style: TextStyle(fontSize: 24)),
+    );
+  }
+}
